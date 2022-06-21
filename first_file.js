da23bd9fac2252fc6a4a4f6d96e76abf64b0627a
Mirera Nyaundi <mirera.moses@gmail.com>
2022-06-21 19:33:24 +0300

    Add files via upload

---
+++ first_file.js
@@ -0,0 +1,11 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+var my_name = 'MIRERA NYAUNDI';
+var height = 158;
+var country = 'KENYA'
+
+console.log(my_name + ' '+ height + ' '+ country);//Write a javascript code that prints out your name, height, and country on the screen
+var my_name = 'MIRERA NYAUNDI';
+var height = 158;
+var country = 'KENYA'
+
+console.log(my_name + ' '+ height + ' '+ country);
