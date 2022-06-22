b11e7492c37ae28aaff9bda293f0705ae66c3e39
mirera <mirera.moses@gmail.com>
2022-06-22 15:58:22 +0300

    initial commit

--- first_file.js
+++ first_file.js
@@ -3,9 +3,5 @@ var my_name = 'MIRERA NYAUNDI';
 var height = 158;
 var country = 'KENYA'
 
-console.log(my_name + ' '+ height + ' '+ country);//Write a javascript code that prints out your name, height, and country on the screen
-var my_name = 'MIRERA NYAUNDI';
-var height = 158;
-var country = 'KENYA'
-
-console.log(my_name + ' '+ height + ' '+ country);
+alert(my_name + ' '+ height + 'cm' + ' '+ country);
+console.log(my_name + ' '+ height + 'cm' + ' '+ country);
